f2bb679992a4e2d83e1145320bf340619c5f4546
Jade <jeffahking@gmail.com>
2018-06-26 23:31:08 -0400

    add neighborhood_id to rent seeds too

--- python_scripts/seeds/rents_seeds.py
+++ python_scripts/seeds/rents_seeds.py
@@ -1,9 +1,10 @@
 import math
 import census_tracts_seeds
+import neighborhoods_seeds
 
 rents_table = 'rents'
 
-def find_tract_match(c, rent):
+def find_tract_and_neighborhood_match(c, rent):
   if rent[2][:5] != "36047":
     print(" - Not Brooklyn CT 36047 - skipping")
     return None
@@ -13,33 +14,47 @@ def find_tract_match(c, rent):
   
   result = c.fetchone()
   if result:
-      return result[0]
+    return {
+      "census_tract_id": result[0],
+      "neighborhood_id": get_neighborhood_id(c, result)[0]
+      }
   else:
     print("  - No tract match found")
     return None
 
+def get_neighborhood_id(c, tract):
+  c.execute('SELECT * FROM {tn} WHERE {cn}={n_id}'\
+    .format(tn=neighborhoods_seeds.neighborhoods_table, cn='id', n_id=tract[1]))
+  return c.fetchone()
+
 def seed_rents(c, rent_csv):
   print("Seeding rents")
   rent_col1 = 'census_tract_id'
-  rent_col2 = 'median_rent_2011'
-  rent_col3 = 'median_rent_2017'
-  rent_col4 = 'median_rent_change_2011_2017'
+  rent_col2 = 'neighborhood_id'
+  rent_col3 = 'median_rent_2011'
+  rent_col4 = 'median_rent_2017'
+  rent_col5 = 'median_rent_change_2011_2017'
 
-  c.execute('CREATE TABLE IF NOT EXISTS {tn} (id INTEGER PRIMARY KEY AUTOINCREMENT, {col1} INTEGER NOT NULL REFERENCES {ct_table}(id), {col2} REAL, {col3} REAL, {col4} REAL)'\
-    .format(tn=rents_table, col1=rent_col1, col2=rent_col2, col3=rent_col3, col4=rent_col4, ct_table=census_tracts_seeds.census_tracts_table))
+  c.execute('CREATE TABLE IF NOT EXISTS {tn} (id INTEGER PRIMARY KEY AUTOINCREMENT, {col1} INTEGER NOT NULL REFERENCES {ct_table}(id), {col2} INTEGER NOT NULL REFERENCES {n_table}(id), {col3} REAL, {col4} REAL, {col5} REAL)'\
+    .format(tn=rents_table, col1=rent_col1, col2=rent_col2, col3=rent_col3, col4=rent_col4, col5=rent_col5, ct_table=census_tracts_seeds.census_tracts_table, n_table=neighborhoods_seeds.neighborhoods_table))
 
   for index, row in enumerate(rent_csv):
     print("rent: " + str(index) + "/" + str(len(rent_csv)))
-    ct_id = find_tract_match(c, row)
-    if ct_id == None:
+    
+    foreign_keys = find_tract_and_neighborhood_match(c, row)
+    if foreign_keys == None:
       continue
+
     if not row[3] or not row[4]:
       continue
-      
+
+    ct_id = foreign_keys["census_tract_id"]
+    n_id = foreign_keys["neighborhood_id"]
+        
     mr_2011 = round(float(row[3]), 2)
     mr_2017 = round(float(row[4]), 2)
 
     change_2011_2017 = round(mr_2017 - mr_2011, 2)
 
-    c.execute('INSERT OR IGNORE INTO {tn} ({col1}, {col2}, {col3}, {col4}) VALUES ({ct_id}, {mr_2011}, {mr_2017}, {change_2011_2017})'\
-      .format(tn=rents_table, col1=rent_col1, col2=rent_col2, col3=rent_col3, col4=rent_col4, ct_id=ct_id, mr_2011=mr_2011, mr_2017=mr_2017, change_2011_2017=change_2011_2017))
+    c.execute('INSERT OR IGNORE INTO {tn} ({col1}, {col2}, {col3}, {col4}, {col5}) VALUES ({ct_id}, {n_id}, {n_id}{mr_2011}, {mr_2017}, {change_2011_2017})'\
+      .format(tn=rents_table, col1=rent_col1, col2=rent_col2, col3=rent_col3, col4=rent_col4, col5=rent_col5, ct_id=ct_id, n_id=n_id, mr_2011=mr_2011, mr_2017=mr_2017, change_2011_2017=change_2011_2017))
--- python_scripts/seeds/seed_db.py
+++ python_scripts/seeds/seed_db.py
@@ -21,7 +21,7 @@ c.execute('pragma foreign_keys=on;')
 def drop_tables():
   # c.execute('DROP TABLE IF EXISTS {tn}'.format(tn=permits_seeds.permits_table))
   # c.execute('DROP TABLE IF EXISTS {tn}'.format(tn=sales_seeds.sales_table))
-  # c.execute('DROP TABLE IF EXISTS {tn}'.format(tn=rents_seeds.rents_table))
+  c.execute('DROP TABLE IF EXISTS {tn}'.format(tn=rents_seeds.rents_table))
   # c.execute('DROP TABLE IF EXISTS {tn}'.format(tn=incomes_seeds.incomes_table))
   # c.execute('DROP TABLE IF EXISTS {tn}'.format(tn=violations_seeds.violations_table))
   # c.execute('DROP TABLE IF EXISTS {tn}'.format(tn=buildings_seeds.buildings_table))
@@ -34,7 +34,7 @@ drop_tables()
 # building_json = json.load(open('data/buildings_data/processed_mappluto.geojson'))
 # violations_json = json.load(open('data/violations_data/bk_violation_data_2008_2017.json'))
 # incomes_csv = list(csv.reader(open("data/demographic_data/bk_median_income_2017.csv")))[1:]
-# rents_csv = list(csv.reader(open("data/rent_data/censustract-medianrentall2017.csv")))[1:]
+rents_csv = list(csv.reader(open("data/rent_data/censustract-medianrentall2017.csv")))[1:]
 # sales_csv = list(csv.reader(open("data/sales_data/bk_property_sales_2011_2017.csv")))[1:]
 # permits_csv = list(csv.reader(open("data/permit_data/processed_bk_permit_data_2011_2017.csv")))[1:]
 
@@ -45,14 +45,14 @@ drop_tables()
 # buildings_seeds.seed_buildings(c, building_json) # Takes about half an hour due ot foriegn key indexing by geodata
 # violations_seeds.seed_violations(c, violations_json)
 # incomes_seeds.seed_incomes(c, incomes_csv)
-# rents_seeds.seed_rents(c, rents_csv)
+rents_seeds.seed_rents(c, rents_csv)
 # sales_seeds.seed_sales(c, sales_csv)
 # permits_seeds.seed_permits(c, permits_csv)
 
 print("Seeding complete.")
 
 # Test DB
-c.execute('SELECT * FROM incomes')
+c.execute('SELECT * FROM rents')
 all_rows = c.fetchall()
 print(all_rows[:1])
 

a697efbd4a479c4bfdf34a78e08e4467c1c312e1
Jade <jeffahking@gmail.com>
2018-07-01 21:00:26 -0400

    transfer over CT layer styles to react

--- client-react/src/LeafletMap/LayersMenu/index.js
+++ client-react/src/LeafletMap/LayersMenu/index.js
@@ -10,6 +10,21 @@ import {
   GeoJSON,
   TileLayer
 } from 'react-leaflet'
+
+import {
+  incomeMedianLayerStyle,
+  incomeChangeLayerStyle,
+  rentMedianLayerStyle,
+  rentChangeLayerStyle,
+  violationsPerBuildingLayerStyle,
+  serviceCallsTotalLayerStyle,
+  serviceCallsPercentViolationLayerStyle,
+  salesTotalLayerStyle,
+  salesWithViolationTotalLayerStyle,
+  salesWithViolationPercentLayerStyle,
+  violationsCountBeforeSaleLayerStyle,
+  permitsTotalLayerStyle
+} from '../GeoJsonStyles'
 const { BaseLayer, Overlay } = LayersControl
 
 export default class LayersMenu extends Component {
@@ -22,49 +37,186 @@ export default class LayersMenu extends Component {
 
     return (
       <LayersControl position={this.props.position}>
-        <BaseLayer checked name="Census Tracts">
+        <BaseLayer checked name="Median Income, 2017">
           <LayerGroup>
             {this.props.store.censusTracts.features.map((feature, index) => {
               return (
                 <GeoJSON
                   key={`ct-${index}`}
-                  color="white"
-                  opacity={1}
-                  weight={1}
-                  fillOpacity={0.7}
-                  fillColor="pink"
                   data={feature['geometry']}
+                  {...incomeMedianLayerStyle(feature.properties.medianIncome2017, feature.properties.totalBuildings)}
                 />
               )
             })}
           </LayerGroup>
         </BaseLayer>
-        <BaseLayer name="Census Tracts 2011">
+        <BaseLayer name="Income Change, 2011 - 2017">
           <LayerGroup>
             {this.props.store.censusTracts.features.map((feature, index) => {
               return (
                 <GeoJSON
                   key={`ct-${index}`}
-                  color="white"
-                  opacity={1}
-                  weight={1}
-                  fillOpacity={0.7}
-                  fillColor="pink"
                   data={feature['geometry']}
+                  {...incomeChangeLayerStyle(
+                    feature.properties.medianIncomeChange20112017,
+                    feature.properties.totalBuildings
+                  )}
                 />
               )
             })}
           </LayerGroup>
         </BaseLayer>
-        <Overlay name="Marker with popup">
-          <Marker position={center}>
-            <Popup>
-              <span>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </span>
-            </Popup>
-          </Marker>
-        </Overlay>
+        <BaseLayer name="Median Rent, 2017">
+          <LayerGroup>
+            {this.props.store.censusTracts.features.map((feature, index) => {
+              return (
+                <GeoJSON
+                  key={`ct-${index}`}
+                  data={feature['geometry']}
+                  {...rentMedianLayerStyle(feature.properties.medianRent2017, feature.properties.totalBuildings)}
+                />
+              )
+            })}
+          </LayerGroup>
+        </BaseLayer>
+        <BaseLayer name="Rent Change, 2011 - 2017">
+          <LayerGroup>
+            {this.props.store.censusTracts.features.map((feature, index) => {
+              return (
+                <GeoJSON
+                  key={`ct-${index}`}
+                  data={feature['geometry']}
+                  {...rentChangeLayerStyle(
+                    feature.properties.medianRentChange20112017,
+                    feature.properties.totalBuildings
+                  )}
+                />
+              )
+            })}
+          </LayerGroup>
+        </BaseLayer>
+        <BaseLayer name="Violations per Building, 2011 - 2017">
+          <LayerGroup>
+            {this.props.store.censusTracts.features.map((feature, index) => {
+              return (
+                <GeoJSON
+                  key={`ct-${index}`}
+                  data={feature['geometry']}
+                  {...violationsPerBuildingLayerStyle(
+                    feature.properties.violationsPerBuilding,
+                    feature.properties.totalBuildings
+                  )}
+                />
+              )
+            })}
+          </LayerGroup>
+        </BaseLayer>
+        <BaseLayer name="Total Service Calls, 2011 - 2017">
+          <LayerGroup>
+            {this.props.store.censusTracts.features.map((feature, index) => {
+              return (
+                <GeoJSON
+                  key={`ct-${index}`}
+                  data={feature['geometry']}
+                  {...serviceCallsTotalLayerStyle(
+                    feature.properties.totalServiceCalls,
+                    feature.properties.totalBuildings
+                  )}
+                />
+              )
+            })}
+          </LayerGroup>
+        </BaseLayer>
+        <BaseLayer name="Percent Service Calls with Violation">
+          <LayerGroup>
+            {this.props.store.censusTracts.features.map((feature, index) => {
+              return (
+                <GeoJSON
+                  key={`ct-${index}`}
+                  data={feature['geometry']}
+                  {...serviceCallsPercentViolationLayerStyle(
+                    feature.properties.percentServiceCallsWithViolation,
+                    feature.properties.totalBuildings
+                  )}
+                />
+              )
+            })}
+          </LayerGroup>
+        </BaseLayer>
+        <BaseLayer name="Total Sales, 2011 - 2017">
+          <LayerGroup>
+            {this.props.store.censusTracts.features.map((feature, index) => {
+              return (
+                <GeoJSON
+                  key={`ct-${index}`}
+                  data={feature['geometry']}
+                  {...salesTotalLayerStyle(feature.properties.totalSales, feature.properties.totalBuildings)}
+                />
+              )
+            })}
+          </LayerGroup>
+        </BaseLayer>
+        <BaseLayer name="Total Sales with Prior Violations, 2011-2017">
+          <LayerGroup>
+            {this.props.store.censusTracts.features.map((feature, index) => {
+              return (
+                <GeoJSON
+                  key={`ct-${index}`}
+                  data={feature['geometry']}
+                  {...salesTotalLayerStyle(
+                    feature.properties.totalSalesPriorViolations,
+                    feature.properties.totalBuildings
+                  )}
+                />
+              )
+            })}
+          </LayerGroup>
+        </BaseLayer>
+        <BaseLayer name="Avg Sales with Prior Violations, 2011-2017">
+          <LayerGroup>
+            {this.props.store.censusTracts.features.map((feature, index) => {
+              return (
+                <GeoJSON
+                  key={`ct-${index}`}
+                  data={feature['geometry']}
+                  {...salesWithViolationPercentLayerStyle(
+                    feature.properties.avgSalesPriorViolations,
+                    feature.properties.totalBuildings
+                  )}
+                />
+              )
+            })}
+          </LayerGroup>
+        </BaseLayer>
+        <BaseLayer name="Avg # of violations 3 years before sale, 2011-2017">
+          <LayerGroup>
+            {this.props.store.censusTracts.features.map((feature, index) => {
+              return (
+                <GeoJSON
+                  key={`ct-${index}`}
+                  data={feature['geometry']}
+                  {...violationsCountBeforeSaleLayerStyle(
+                    feature.properties.avgViolationCount3YearsBeforeSale,
+                    feature.properties.totalBuildings
+                  )}
+                />
+              )
+            })}
+          </LayerGroup>
+        </BaseLayer>
+        <BaseLayer name="Total Permits, 2011 - 2017">
+          <LayerGroup>
+            {this.props.store.censusTracts.features.map((feature, index) => {
+              return (
+                <GeoJSON
+                  key={`ct-${index}`}
+                  data={feature['geometry']}
+                  {...permitsTotalLayerStyle(feature.properties.totalPermits, feature.properties.totalBuildings)}
+                />
+              )
+            })}
+          </LayerGroup>
+        </BaseLayer>
         <Overlay checked name="Layer group with circles">
           <LayerGroup>
             <Circle center={center} fillColor="blue" radius={200} />

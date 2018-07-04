77307329585a893002fd03eb9450d7b81af0d4d2
Jade <jeffahking@gmail.com>
2018-07-04 17:16:46 -0400

    dirnames

--- server/routes/neighborhoodRoutes.js
+++ server/routes/neighborhoodRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const NeighborhoodController = require(__dirname + '/../controllers/ceighborhoodController')
+const NeighborhoodController = require(__dirname + '/../controllers/neighborhoodController')
 
 router.get('/', NeighborhoodController.index)
 module.exports = router
